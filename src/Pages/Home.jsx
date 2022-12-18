5c366916f33851e760cca7259be18a1bf8cf4edf
Anshika Gautam <anshikagautam130@gmail.com>
2022-12-18 23:53:48 +0530

    heading main animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,33 +6,27 @@ const Home = () => {
   return (
     <div data-scroll data-scroll-speed="7" data-scroll-position="top">
       <main>
-            <h1 className="heading_main">
-                <div className="animated_heading">
-                    <div className="word-1">
-                        <div>Industrial</div>
-                    </div>
-                    <div className="word-2">
-                        <div>Interaction</div>
-                    </div>
-                    <div className="word-3">
-                        <div>Visual</div>
-                    </div>
-                </div>
+        <h1 className="heading_main">
+          <div className="animated_heading_wrapper">
+            <span>Visual</span>
+            <span>Industrial</span>
+            <span>Interaction</span>
+            <span>Visual</span>
+          </div>
 
-                {/* <div className="animated_heading">
-                    <span>Industrial</span>
-                    <span>Interaction</span>
-                    <span>Visual</span>
-                </div>  */}
-                <span>Designer</span>
-            </h1>
-            <p>
-                I am an Industrial and Interaction Designer who creates designs
-                that are both sustainable and universal. I try to draw inspiration
-                from music and comics in order to create designs that provide
-                a harmonious and consistent user experience for others.
-            </p>
-        </main>
+          <span>Designer</span>
+        </h1>
+        <p>
+          Hi there,
+          <br />
+          I, Saksham Parag Agarwal am an Industrial Design Student from <b style={{fontWeight: '400'}}>Delhi
+          Technological University</b>. I also have a flair for Interaction and
+          Visual design works. I like to create designs that are both universal
+          sustainable. <br /> I try to draw inspiration from music and comics in order
+          to create designs that provide a harmonious and consistent user
+          experience for others.
+        </p>
+      </main>
 
       <section className="projects">
         <div className="section_title">
