b51662dfe86161e07f8a51a63c39443f0d8806b4
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-01-22 15:34:33 +0530

    Config to rewrite index.html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html'
+        }
+        next()
+      })
+    },
+  }
+}
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   appType: 'mpa',
   base: '/',
-  plugins: [react()],
+  plugins: [react(), rewriteSlashToIndexHtml()],
 })
