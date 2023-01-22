6f53a31f99b42e6c2321b3146ae758ab5612ca10
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-01-22 15:35:19 +0530

    rewrite slash

--- src/Components/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Project} from "./Projects/Project"
-export {default as Navbar} from './Navbar/Navbar'
-export {default as SimpleNav} from './Navbar/SimpleNav'
-export {default as Main} from './HeaderComp/Main'
-export {default as AllProjects} from './Projects/AllProjects'
-export {default as Collections} from './Collections/Collections'
