ae2bc266ca2f8557986513f39f40f15495f04225
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-01-24 22:21:15 +0530

    temp

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,15 +1,31 @@
 import React, { useRef, useState } from "react";
 import "./project.css";
 
-const Project = ({data}) => {
+const Project = ({ data }) => {
+  const [isTouched, setIsTouched] = useState(false);
+
+  const handleTouchStart = () => {
+    setIsTouched(true);
+  };
+
+  const handleTouchEnd = () => {
+    setIsTouched(false);
+  };
+
   return (
-    <li className="project" style={{backgroundImage: `url(${data.thumbnail})`}}>
+    <li
+      className={isTouched ? "project touched" : "project"}
+      style={{ backgroundImage: `url(${data.thumbnail})` }}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className="project_container">
-      <div className="project__title">{ data.title }</div>
+        <div className="project__title">{data.title}</div>
       </div>
       <div className="project_details">
-        <div className="left">Project Duration - { data.duration }</div>
-        <div className="right">{ data.period }</div>
+        <div className="left">Project Duration - {data.duration}</div>
+        <div className="right">{data.period}</div>
       </div>
     </li>
   );
