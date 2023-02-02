66d996801815ed4a51b84293cb0e7a762628b9c3
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-02-02 22:27:31 +0530

    New changes

--- src/Components/HeaderComp/Main.jsx
+++ src/Components/HeaderComp/Main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import './main.css'
+import "./main.css";
 
-const Main = () => {
+const Main = ({compRef}) => {
+  const handleClickToScroll = () => {
+    compRef.current.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <main>
       <h1 className="heading_main">
@@ -14,12 +17,31 @@ const Main = () => {
 
         <span>Designer</span>
       </h1>
-      <p>
-        I am an Industrial and Interaction Designer who creates designs that are
-        both sustainable and universal. I try to draw inspiration from music and
-        comics in order to create designs that provide a harmonious and
-        consistent user experience for others.
-      </p>
+      <div className="hero__content">
+        <p>
+          I am an Industrial and Interaction Designer who creates designs that
+          are both sustainable and universal. I try to draw inspiration from
+          music and comics in order to create designs that provide a harmonious
+          and consistent user experience for others.
+        </p>
+        <div className="hero__scroll_arrow" onClick={handleClickToScroll}>
+          <svg
+            width="93"
+            height="74"
+            viewBox="0 0 93 74"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {/* <circle cx={0} cy={0} r={100} stroke="white" strokeWidth={1} /> */}
+            <path
+              d="M46.0053 0.683594V73.3168M92.2717 26.4772L46.1196 73.3167L1.23145 26.0555"
+              stroke="white"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
     </main>
   );
 };
