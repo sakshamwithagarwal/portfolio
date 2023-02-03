99c14bc5f3eac5ef1962f589ff024a935b654523
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-02-03 22:44:06 +0530

    custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Collection, Home } from "./Pages";
-import { Noise, SplashScreen } from "./Components";
+import { Noise, SplashScreen, Cursor } from "./Components";
 import "./App.css";
+import { useRef } from "react";
+import { gsap } from "gsap";
 
 const router = createBrowserRouter(
   [
@@ -18,11 +20,48 @@ const router = createBrowserRouter(
 );
 
 function App() {
-  return (
-    <div className="App">
-      <Noise />
-      {/* <SplashScreen /> */}
+  // const cursorRef = {
+  //   cursorBigCircle: useRef(null),
+  //   cursorSmallCircle: useRef(null),
+  // };
+  const cursorBigCircle = useRef(null);
+  const cursorSmallCircle = useRef(null);
 
+  const moveCursor = (e) => {
+    gsap.to(cursorBigCircle.current, {
+      x: e.pageX - 15,
+      y: e.pageY - 15,
+      duration: 0.4,
+    });
+
+    gsap.to(cursorSmallCircle.current, {
+      x: e.pageX - 5,
+      y: e.pageY - 7,
+      duration: 0.1,
+    });
+  };
+
+  return (
+    <div className="App" onMouseMove={moveCursor}>
+      {/* <Cursor ref={cursorRef} /> */}
+
+      <div className="cursor">
+        <div className="cursor__ball cursor__ball--big" ref={cursorBigCircle}>
+          <svg height="30" width="30">
+            <circle cx="15" cy="15" r="12" stroke-width="0"></circle>
+          </svg>
+        </div>
+        <div
+          className="cursor__ball cursor__ball--small"
+          ref={cursorSmallCircle}
+        >
+          <svg height="10" width="10">
+            <circle cx="5" cy="5" r="4" stroke-width="0"></circle>
+          </svg>
+        </div>
+      </div>
+
+      <Noise />
       <div className="line-1"></div>
       <div className="line-2"></div>
       <div className="line-3"></div>
