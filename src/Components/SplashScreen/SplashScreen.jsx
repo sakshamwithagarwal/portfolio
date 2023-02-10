f81dbe3a4b057cebb86519ce268ddc3d75a1b572
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-02-10 21:08:17 +0530

    splash screen ui change

--- src/Components/SplashScreen/SplashScreen.jsx
+++ src/Components/SplashScreen/SplashScreen.jsx
@@ -4,38 +4,25 @@ import { gsap } from "gsap";
 
 const SplashScreen = ({ handleClick }) => {
   const splashRef = useRef(null);
-  // const animateBottom = () => {
-  //   gsap.from(clickRef.current, {
-  //     y: 100
-  //   });
-  //   gsap.to(clickRef.current, {
-  //     y: 0,
-  //     duration: .6,
-  //   });
-  // };
-  // useEffect(() => {
-  //   animateBottom();
+  // useLayoutEffect(() => {
+  //   const ctx = gsap.context(() => {
+  //     gsap.fromTo(
+  //       ".splash__screen-info",
+  //       {
+  //         y: 100,
+  //         scale: 0.5,
+  //       },
+  //       {
+  //         y: 0,
+  //         scale: 1,
+  //         delay: 1,
+  //         duration: 0.5,
+  //       }
+  //     );
+  //   }, splashRef);
+
+  //   return () => ctx.revert();
   // }, []);
-
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      gsap.fromTo(
-        ".splash__screen-info",
-        {
-          y: 100,
-          scale: 0.5,
-        },
-        {
-          y: 0,
-          scale: 1,
-          delay: 1,
-          duration: 0.5,
-        }
-      );
-    }, splashRef);
-
-    return () => ctx.revert();
-  }, []);
 
   return (
     <div
@@ -55,8 +42,30 @@ const SplashScreen = ({ handleClick }) => {
         <h1>Hey, I am Saksham Parag Agarwal</h1>
         <h3>I am an Experience Designer</h3>
       </div>
+      <div className="splash__screen-info">
+        click to enter{" "}
+        <div className="arrow-circle">
+        <svg
+          width="31"
+          height="8"
+          viewBox="0 0 31 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 3.5C0.723858 3.5 0.5 3.72386 0.5 4C0.5 4.27614 0.723858 4.5 1 4.5V3.5ZM30.3536 4.35355C30.5488 4.15829 30.5488 3.84171 30.3536 3.64645L27.1716 0.464466C26.9763 0.269204 26.6597 0.269204 26.4645 0.464466C26.2692 0.659728 26.2692 0.976311 26.4645 1.17157L29.2929 4L26.4645 6.82843C26.2692 7.02369 26.2692 7.34027 26.4645 7.53553C26.6597 7.7308 26.9763 7.7308 27.1716 7.53553L30.3536 4.35355ZM1 4.5H30V3.5H1V4.5Z"
+            fill="#83878D"
+          />
+        </svg>
+        </div>
+      </div>
 
-      <div className="splash__screen-info">click to enter</div>
+      <div className="splash__screen-links">
+        <ul>
+          <li>cookies</li>
+          <li>legals</li>
+        </ul>
+      </div>
     </div>
   );
 };
