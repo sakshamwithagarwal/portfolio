2b68448ea2e754a6ea40e3dc7a88c3ccbe81ac6f
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-02-13 20:05:04 +0530

    Background splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Collection, Home } from "./Pages";
-import { Noise, SplashScreen, Cursor } from "./Components";
+import { Noise, SplashScreen, Cursor, Background } from "./Components";
 import "./App.css";
 import { useRef, useState, useEffect } from "react";
 import { gsap } from "gsap";
@@ -22,10 +22,7 @@ const router = createBrowserRouter(
 function App() {
   const cursorBigCircle = useRef(null);
   const cursorSmallCircle = useRef(null);
-  const line1Ref = useRef(null);
-  const line2Ref = useRef(null);
-  const line3Ref = useRef(null);
-  const circleRef = useRef(null);
+  
   const [isSplashOpen, setIsSplashOpen] = useState(true);
 
   const onClickHandler = () => {
@@ -54,28 +51,7 @@ function App() {
     );
   };
 
-  useEffect(() => {
-    gsap.fromTo(
-      line1Ref.current,
-      { height: 0 },
-      { height: "100%", duration: 1.4, delay: 0.6 }
-    );
-    gsap.fromTo(
-      line2Ref.current,
-      { height: 0 },
-      { height: "100%", duration: 1.4, delay: 0.8 }
-    );
-    gsap.fromTo(
-      line3Ref.current,
-      { height: 0 },
-      { height: "100%", duration: 1.4, delay: 1 }
-    );
-    gsap.fromTo(
-      circleRef.current,
-      { width: 0, height: 0 },
-      { width: "29.27vw", height: "29.27vw", duration: 1, delay: 1.2 }
-    );
-  }, []);
+  
 
   return (
     <div
@@ -103,10 +79,7 @@ function App() {
       </div>
 
       <Noise />
-      <div className="line-1" ref={line1Ref}></div>
-      <div className="line-2" ref={line2Ref}></div>
-      <div className="line-3" ref={line3Ref}></div>
-      <div className="circle" ref={circleRef}></div>
+      <Background />
       <div>
         <RouterProvider router={router} />
       </div>
