64631cabfd333dab9a5231a65c1679bff8429102
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-04-04 15:45:33 +0530

    Lazy loading comp

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,7 +4,7 @@ export {default as SimpleNav} from './Navbar/SimpleNav'
 export {default as Main} from './HeaderComp/Main'
 export {default as AllProjects} from './Projects/AllProjects'
 export {default as Collections} from './Collections/Collections'
-export {default as SplashScreen} from './SplashScreen/SplashScreen'
+// export {default as SplashScreen} from './SplashScreen/SplashScreen'
 export {default as Noise} from './Noise/Noise'
 export {default as Cursor} from './Cursor/Cursor'
 export {default as Background} from "./BackgroundStyle/Background"
