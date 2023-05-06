af7ab45318dafd377db536cb6555bc23a22171d4
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-05-06 18:03:24 +0530

    refactor: ♻️theme functionality switched to APP

--- src/App.jsx
+++ src/App.jsx
@@ -56,13 +56,44 @@ function App() {
     fetchProjects();
   }, []);
 
+  // 🎨 Color theme
+  const getColorPreference = () => {
+    if (localStorage.getItem(storageKey))
+      return localStorage.getItem(storageKey);
+    else
+      return window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+  };
+
+  const theme = { value: getColorPreference() };
+
+  const setPreference = () => {
+    localStorage.setItem(storageKey, theme.value);
+    reflectPreference();
+  };
+
+  const reflectPreference = () => {
+    document.firstElementChild.setAttribute("data-theme", theme.value);
+
+    document
+      .querySelector("#theme-toggle")
+      ?.setAttribute("aria-label", theme.value);
+  };
+
+  reflectPreference();
+
+  window.onload = () => {
+    reflectPreference();
+  };
+
   const router = createBrowserRouter(
     [
       {
         path: "/",
         element: (
           <Suspense fallback={<>loading...</>}>
-            <Home projects={projectData} />,
+            <Home onClick={onclick} projects={projectData} />,
           </Suspense>
         ),
       },
@@ -107,7 +138,7 @@ function App() {
       </BrowserView>
       <Noise />
       {isLoading ? (
-        <Preloader />
+        <Preloader theme={theme} />
       ) : (
         <>
           <div>
