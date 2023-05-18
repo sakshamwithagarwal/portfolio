c52972d3a02510eb1952d2d146d0fdf50485f10d
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2023-05-18 19:47:34 +0530

    refactor: ♻Individual Project query modification

--- src/Pages/ExpandedProject/ExpandedProject.jsx
+++ src/Pages/ExpandedProject/ExpandedProject.jsx
@@ -2,23 +2,58 @@ import React, { useEffect, useState } from "react";
 import locomotiveScroll from "locomotive-scroll";
 import { useParams } from "react-router-dom";
 import { motion as m } from "framer-motion";
+import { request } from "graphql-request";
 import "./project.css";
 
 const ExpandedProject = ({ projects, isOpen, setIsOpen }) => {
   const { slug } = useParams();
   const [project, setProject] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const projectData = await projects.find(
-        (project) => project.slug === slug
-      );
-      setProject(projectData);
-      console.log(project);
-    })();
+  const projectQuery = {
+    PROJECT_QUERY: `
+    {
+      project(where: {slug: "${slug}"}) {
+        id
+        projectName
+        projectThumbnail {
+          url
+        }
+        slug
+        projectContent {
+          html
+        }
+      }
+    }
+  `,
+    endPointURL:
+      "https://api-ap-south-1.hygraph.com/v2/clha5gtcw11sx01taepog266q/master",
+  };
 
-    return () => {};
-  }, []);
+    // 📨 Fetch project
+    useEffect(() => {
+      const fetchProjects = async () => {
+        const { project } = await request(
+          projectQuery.endPointURL,
+          projectQuery.PROJECT_QUERY
+        );
+        setProject(project);
+      };
+  
+      fetchProjects();
+      return () => {setProject(null)};
+    }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const projectData = await projects.find(
+  //       (project) => project.slug === slug
+  //     );
+  //     setProject(projectData);
+  //     console.log(project);
+  //   })();
+
+  //   return () => {};
+  // }, []);
 
   return (
     <m.div
@@ -27,7 +62,7 @@ const ExpandedProject = ({ projects, isOpen, setIsOpen }) => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.25, delay: 0.5, ease: "easeOut" }}
     >
-      {!isOpen && project && project ? (
+      {!isOpen && project ? (
         <div
           className="project-main"
           // data-scroll
